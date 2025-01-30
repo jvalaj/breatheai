a26ba6a17ec32076ad7ced4ceebf8ff86ac5faa2
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2025-01-29 20:20:27 -0500

    setup frontend with next

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def analyze():
         "lung_health_index": 85,
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    app.run(debug=True)
